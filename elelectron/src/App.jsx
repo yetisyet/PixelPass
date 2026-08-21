d004ab34eb5e36954815ac25c9910e2806d8417f
Noxy 3 <thatguynoxxie@gmail.com>
2026-08-21 20:05:12 +1000

    Electron React initial set up

---
+++ elelectron/src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold">MEOW</h1>
+    </main>
+  )
+}
